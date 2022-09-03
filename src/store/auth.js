7813e772a5bbe412c08059d567e1dc43d9a96790
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-09-03 14:19:39 +0300

    add redux-toolkit

---
+++ src/store/auth.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState ={
+    user:false
+}
+
+export const auth = createSlice({  
+    name: 'auth',
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user =action.payload
+
+        },
+        logout : (state, action) => {
+            state.user = false
+        },
+
+
+    },
+});
+
+export const { login, logout } = auth.actions;
+export default auth.reducer;
+
